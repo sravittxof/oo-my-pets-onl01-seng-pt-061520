12bcf6e017cc2d2a7d7cb6d4d87a5d95af43b0c9
Travis Fox <travis.fox90@gmail.com>
2020-07-21 00:50:57 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,22 @@
 class Cat
-  # code goes here
+  
+  attr_accessor :owner, :mood
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @owner = owner
+    @mood = "nervous"
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,52 @@
 class Owner
-  # code goes here
+  
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name="")
+    @species = "human"
+    save
+  end
+  
+  def say_species
+    "I am a human"
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.count
+    self.all.count
+  end
+  
+  def self.reset_all
+    self.all.clear
+  end
+  
+  def cats
+    my_cats = Cat.all.select { |owner| owner == self }
+  end
+  
+  def dogs
+    my_dogs = Dog.all.select { |owner| owner == self }
+  end
+  
+  def buy_cat(cat_name)
+    Cat.new(cat_name, self)
+  end
+  
+  def buy_dog(dog_name)
+    Dog.new(dog_name, self)
+  end
+  
+  def walk_dogs
+    self.dogs.each do { |dog| dog.mood = "happy"
+  end
+  
 end
